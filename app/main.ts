eeb193e4671d1197d5510e6ec547bf0e83bfbfec
MickeyZhaang <mickey.zhang@vizcom.com>
2026-08-25 20:12:48 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -13,6 +13,14 @@ rl.on("line", (comm) => {
     return;
   } else if (comm.startsWith("echo ")) {
     console.log(comm.slice(5));
+  } else if (comm.startsWith("type ")) {
+    const types = new Set(["exit", "echo", "type"]);
+    const command = comm.slice(5);
+    if (command in types) {
+      console.log(`${comm} is a shell builtin`);ß
+    } else {
+      console.log(`${comm}: not found`);
+    }
   } else {
     console.log(`${comm}: command not found`);
   }
